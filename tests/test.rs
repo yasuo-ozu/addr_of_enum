912c206eab5ff4dde345c6b1bb05d510fadb2b00
yozu <yasuo@ozu.email>
2023-12-30 19:15:31 +0900

    Add uninhabited tesst

--- tests/test.rs
+++ tests/test.rs
@@ -1,3 +1,4 @@
+use core::convert::Infallible;
 use enum_offset::{enum_offset, EnumOffset};
 
 #[derive(EnumOffset)]
@@ -12,6 +13,7 @@ fn test() {
         item1: 1,
         item2: 2u8,
     };
+    let e1a: E<Infallible> = E::E1(1, 2, 3);
     match &e1 {
         E::E1(item1, item2, item3) => {
             assert_eq!(item1 as *const usize, enum_offset!(&e1, E1, 0));
@@ -24,6 +26,15 @@ fn test() {
         E::E2 { item1, item2 } => {
             assert_eq!(item1 as *const u32, enum_offset!(&e2, E2, item1));
             assert_eq!(item2 as *const u8, enum_offset!(&e2, E2, item2));
+        }
+        _ => panic!(),
+    }
+    // uninhabited
+    match &e1a {
+        E::E1(item1, item2, item3) => {
+            assert_eq!(item1 as *const usize, enum_offset!(&e1a, E1, 0));
+            assert_eq!(item2 as *const u8, enum_offset!(&e1a, E1, 1));
+            assert_eq!(item3 as *const u16, enum_offset!(&e1a, E1, 2));
         }
         _ => panic!(),
     }
